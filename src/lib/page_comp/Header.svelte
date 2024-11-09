c24aaab5f89bbc185f7c51d110961f49771f4264
KineticJetIce245 <kineticjetice245@gmail.com>
2024-11-09 08:04:50 -0500

    button

--- src/lib/page_comp/Header.svelte
+++ src/lib/page_comp/Header.svelte
@@ -1,20 +1,64 @@
 <script>
   /* load colors */
   import Button from "$lib/page_item/Button.svelte";
-  
+
+  /* load texts */
+  import header_json from "$lib/storage/comp_info/header.json";
+
   import colors_json from "$lib/storage/color_palette.json";
   const COLORS = colors_json;
+  const TEXT = header_json;
 
+  const BUTTON_PARA = {
+    text: "16px",
+    text_margin: "6px",
+    hitbox_width: "100px",
+    hitbox_height: "40px",
+    border_radius: "20px",
+  };
 </script>
 
 <!-- Outermost container which allows overflowing -->
-<div style="width: 100$;">
+<div>
   <!-- Navigation Bar -->
-  <div style="width: 100%; margin: 0px">
-    <Button color_theme={COLORS.button.type_black}
-      text_size="10px"
-      text_val="hi"/>
+  <div id="nav_bar">
+    <div id="nav_button_list">
+    <div class="nav_button" id="about_button">
+      <Button color_theme={COLORS.button.type_black}
+        text_val={TEXT.nav_bar.buttons.about}
+        size_info={BUTTON_PARA}
+      />
+    </div>
+    <div class="nav_button" id="sponsors_button">
+      <Button color_theme={COLORS.button.type_black}
+        text_val={TEXT.nav_bar.buttons.sponsors}
+        size_info={BUTTON_PARA}
+      />
+    </div>
+    <div class="nav_button" id="faq_button">
+      <Button color_theme={COLORS.button.type_black}
+        text_val={TEXT.nav_bar.buttons.FAQ}
+        size_info={BUTTON_PARA}
+      />
+    </div>
+    </div>
   </div>
   <div class="floating_div">
   </div>
 </div>
+
+<style>
+div {
+  width: 100%;
+}
+
+.nav_button {
+  margin: 2px;
+}
+
+#nav_button_list {
+  margin: 10px;
+  display: flex;
+  flex-direction: row;
+}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,23 +13,26 @@
   margin: 0px;
   background-color: black;
   ">
-  <div id="header_container">
-    <Header />
-  </div>
   <div id="background_image_container">
     <img id="star_map" src={star_map} alt="Background Star" />
+  </div>
+  <div id="header_container">
+    <Header />
   </div>
 </body>
 
 <style>
 #star_map {
+  object-fit: cover;
   width: 100%;
-  height: auto;
+  height: 100%;
 }
 #background_image_container {
   width: 100%;
+  height: 100%;
   position: fixed;
 }
 #header_container {
+  position: fixed;
 }
 </style>
